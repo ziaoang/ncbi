3329e7410bb78d458b6162e490fb70bbe979275f
ziaoang <ziaoang@gmail.com>
2016-08-19 02:42:36 +0800

    rm bugs

--- yuntsg/src/pack.py
+++ yuntsg/src/pack.py
@@ -6,10 +6,11 @@ if not os.path.exists("pdf/pack/need_to_rename"):
 cnt = 0
 for line in open("data/retrieval.txt"):
     query, pmid, title = line.strip().split('\t')
-    if not os.path.exists("pdf/pack/need_to_rename/%s.pdf"):
-        cnt += 1
-        order = "cp pdf/merge/%s.pdf pdf/pack/need_to_rename/%s.pdf" % (pmid, pmid)
-        os.system(order)
+    if os.path.exists("pdf/merge/%s.pdf" % pmid):
+        if not os.path.exists("pdf/pack/need_to_rename/%s.pdf" % pmid):
+            cnt += 1
+            order = "cp pdf/merge/%s.pdf pdf/pack/need_to_rename/%s.pdf" % (pmid, pmid)
+            os.system(order)
 
 print '-' * 20
 print "pack %d papers" % cnt
